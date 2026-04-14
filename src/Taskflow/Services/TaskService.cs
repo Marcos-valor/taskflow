95ebb26f5dfce914fe0656ab975b158673a7a30d
Marcos-valor <marcosvalor.19@gmail.com>
2026-04-14 14:27:44 -0300

    agregar crear tarea

--- src/Taskflow/Services/TaskService.cs
+++ src/Taskflow/Services/TaskService.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
+using Taskflow.Models;
 
-namespace Taskflow.Services
+namespace TaskFlow.Services
 {
-    internal class TaskService
+    public class TaskService
     {
+        private List<TaskItem> _tasks = new List<TaskItem>();
+
+        public void CrearTarea(string titulo, string descripcion, string responsable)
+        {
+            var tarea = new TaskItem
+            {
+                Id = _tasks.Count + 1,
+                Titulo = titulo,
+                Descripcion = descripcion,
+                Responsable = responsable,
+                Estado = "Pendiente",
+                FechaCreacion = DateTime.Now
+            };
+
+            _tasks.Add(tarea);
+            Console.WriteLine($"\n✓ Tarea '{titulo}' creada correctamente con ID {tarea.Id}");
+        }
     }
 }
